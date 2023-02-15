0e1e4bb90a0b1acd7a335a5aada560f9d8e919e1
Ulises Pirra <123274098+UlisesPirra@users.noreply.github.com>
2023-02-15 15:36:44 -0300

    Commit de pagina completa

---
+++ JS/scrip.js
@@ -0,0 +1,19 @@
+
+ 
+    document.getElementById('btn1').addEventListener('focus',function() {
+      
+       document.getElementById('btn1').click();
+       })  
+
+    
+    document.getElementById('btn2').addEventListener('focus',function() {
+      
+        document.getElementById('btn2').click();
+        })  
+
+    
+    document.getElementById('btn3').addEventListener('focus',function() {
+      
+         document.getElementById('btn3').click();
+         })  
+        
